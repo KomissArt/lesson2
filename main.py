b30d69b741fb72bf17afaeba2ae0f2f9a70df9e3
KomissArt <komisst6@gmail.com>
2024-10-10 01:17:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+home_work=12
+hours= 1.5
+type_of_course='Python'
+one_task_time = hours/home_work
+print('Курс:',type_of_course,'; всего задач:',home_work, '; затрачено часов:', hours, '; среднее время выполнения:', one_task_time, 'часов'  )
